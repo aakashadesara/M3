b9297b701dc5594a98337432027c118f506a05ae
Aakash Adesara <aakash.adesara@gmail.com>
2014-10-12 00:57:43 -0700

    Update leap.js

--- leap.js
+++ leap.js
@@ -2,7 +2,7 @@
  * LeapJS v0.6.2                                                  
  * http://github.com/leapmotion/leapjs/                                        
  *                                                                             
- * Copyright 2013 LeapMotion, Inc. and other contributors                      
+ * Copyright  2013 LeapMotion, Inc. and other contributors                      
  * Released under the BSD-2-Clause license                                     
  * http://github.com/leapmotion/leapjs/blob/master/LICENSE.txt                 
  */
